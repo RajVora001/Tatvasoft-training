37d606c0e99266068c7028e7d218c26a901a6fd4
RajVora001 <voraraj726@gecg28.ac.in>
2023-03-10 18:30:14 +0530

    first commit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -9,16 +9,16 @@ export class LoginForm extends Component {
 
           </div>
           <div className="header">
-            <img src="#" alt="logo" /> 
+            <img src="#" alt="logo" className='logo' /> 
             <a href="#" className='Login'>Login</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
             <a href="#" className='Login'>Register</a>
             &nbsp;&nbsp;&nbsp;<button className='cart'>Cart</button>
           </div>
           <div className="search">
-            
+           <input type="text" name="" placeholder='What are you looking for...' className='searchbox' />
           </div>
           
-           
+          
           
         </>
     )
